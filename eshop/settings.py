69cbf72b001269199af7cf5512a0cb3062c65d9f
ayush <aks010598@gmail.com>
2020-08-16 13:21:55 +0530

    addied media file display code

--- eshop/settings.py
+++ eshop/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'file/upl/oad/x/y/qwerty/'
+MEDIA_PATH = BASE_DIR
--- eshop/urls.py
+++ eshop/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from eshop.settings import MEDIA_URL,MEDIA_PATH
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls')),
-]
+]+static(MEDIA_URL,document_root=MEDIA_PATH)
