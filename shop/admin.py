d8d500f7143f86f340520a02f5f5e87b4d791165
ayush <aks010598@gmail.com>
2020-08-16 12:27:56 +0530

    Admin Panel look modified

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import  format_html
 
 from shop.models.product import ProductImages, Product
 
@@ -8,7 +9,23 @@ class ProductImageModel(admin.StackedInline):
 
 
 class ProductModel(admin.ModelAdmin):
+    list_display = ['id', 'name', 'get_description','get_price','get_discount','get_sale_price','file','thumbnail']
     inlines = [ProductImageModel]
 
+    def get_description(self,obj):
+        return format_html(f'<span title="{obj.description}">{obj.description[0:15]}...</span>')
+
+    def get_price(self,object):
+        return '₹ '+str(object.price)
+
+    def get_discount(self,object):
+        return str(object.discount)+'%'
+
+    def get_sale_price(self,obj):
+        return '₹ '+str(obj.price-(obj.price*obj.discount)/100)
+
+    get_sale_price.short_description = "Sale Price"
+    get_discount.short_description = "Discount"
+    get_price.short_description = "Price"
 
 admin.site.register(Product, ProductModel)
